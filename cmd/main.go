f423a12e883a17bfccb893d3234d7bef93c19241
nikkisversion <nikrc15@gmail.com>
2026-06-02 14:00:53 +0530

    testing for file reader

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/nikkisversion/astral/fileparser"
+)
 
 func main() {
 	fmt.Println("Hello from Astral! LFG!")
+
+	fileReader, err := fileparser.GOFileReader("testFile.go")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer fileReader.Close()
+
+	if _, err := io.Copy(os.Stdout, fileReader); err != nil {
+		fmt.Println("Error:", err)
+	}
+
 }
